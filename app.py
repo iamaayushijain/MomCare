618e782681dbe05a09124adc71ea505c7631d54f
Aayushi Jain <iamaayushirocks@gmail.com>
2024-11-18 18:35:49 +0530

    Firebase issue fixed finally

--- app.py
+++ app.py
@@ -39,29 +39,31 @@ load_dotenv()
 firebase_config = {
     "apiKey": os.getenv("FIREBASE_API_KEY"),
     "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
     "projectId": os.getenv("FIREBASE_PROJECT_ID"),
     "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
     "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
     "appId": os.getenv("FIREBASE_APP_ID"),
     "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
-    "type": "service_account",
+     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Ensure newlines are correctly handled
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT"),
     "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
-    "databaseURL": os.getenv("FIREBASE_DATABASE_URL")
+
 }
 
 
 
 # Initialize Firebase Admin SDK only once
 if not firebase_admin._apps:
+    print(os.getenv("FIREBASE_PRIVATE_KEY"))
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 # Firestore Database
